ebce3a79f09556cb6373cae0f0b1befa67c0725d
yousefelsharkawy26 <121326305+yousefelsharkawy26@users.noreply.github.com>
2022-12-27 03:51:30 +0200

    Add files via upload

---
+++ perfect/perfect/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long num, rem, i, sum = 0;
+            long n1, n2;
+            Console.Write("Enter your First Number : ");
+            n1 = long.Parse(Console.ReadLine());
+            Console.Write("Enter your Second Number : ");
+            n2 = long.Parse(Console.ReadLine());
+            for (num = n1; num <= n2;num++)
+            {
+                for (i = 1; i <= (num/2);i++)
+                {
+                    rem = num % i;
+                    if (rem == 0)
+                    {
+                        sum = sum + i;
+                    }
+                }
+                if (num == sum)
+                    Console.WriteLine("Number {0} is Perfect Number in Range ", num);
+                sum = 0;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ primenumber/primenumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace primenumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sec, i, n1, n2;
+
+            Console.Write("Enter the frist Number : ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second Number : ");
+            n2 = int.Parse(Console.ReadLine());
+
+            if (n1 == 1)
+                n1++;
+            num = n1;
+            Console.WriteLine("Prime in given range is : ");
+            while (num <= n2)
+            {
+                sec = 0;
+                for (i = 2; num > i; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        sec = 1;
+                        break;
+                    }
+                }
+                if (sec == 0)
+                    Console.WriteLine(num);
+                num++;
+                
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
